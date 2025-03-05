a277c95da2e28131766f0913d227b367644167f7
Giulia <giuliaarmanelli24@gmail.com>
2025-03-05 18:35:03 -0300

    feat: implement subscription creation with error handling and repository integration

--- events/src/main/java/br/com/nlw/events/repo/UserRepo.java
+++ events/src/main/java/br/com/nlw/events/repo/UserRepo.java
@@ -3,6 +3,6 @@ package br.com.nlw.events.repo;
 import br.com.nlw.events.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepo extends CrudRepository<User, Interger> {
+public interface UserRepo extends CrudRepository<User, Integer> {
     public User findByEmail (String email);
 }
--- events/src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ events/src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -1,4 +1,52 @@
 package br.com.nlw.events.service;
 
+import br.com.nlw.events.dto.SubscriptionConflictException;
+import br.com.nlw.events.exception.EventNotFoundException;
+import br.com.nlw.events.model.Event;
+import br.com.nlw.events.model.Subscription;
+import br.com.nlw.events.model.User;
+import br.com.nlw.events.repo.EventRepo;
+import br.com.nlw.events.repo.SubscriptionRepo;
+import br.com.nlw.events.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SubscriptionService {
+
+    @Autowired
+    private EventRepo evtRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private SubscriptionRepo subRepo;
+
+
+    public Subscription createNewSubscription (String eventName, User user){
+
+        //recuperar o evento pelo nome
+        Event evt = evtRepo.findByPrettyName(eventName);
+        if (evt == null){ //Evento existe?
+            throw new EventNotFoundException("Evento "+ eventName + " nao existe.");
+        }
+
+        User userRec = userRepo.findByEmail(user.getEmail());
+        if (userRec == null){ //Usuário não existe?
+            userRec = userRepo.save(user);
+        }
+
+        Subscription subs = new Subscription();
+        subs.setEvent(evt);
+        subs.setSubscriber(userRec);
+
+        Subscription tmpSub = subRepo.findByEventAndSubscriber(evt, userRec);
+        if (tmpSub != null){
+            throw new SubscriptionConflictException("Ja existe inscricao para o usuario "+ userRec.getName() +" no evento "+ evt.getTitle() );
+        }
+
+        Subscription res = subRepo.save(subs);
+        return res;
+    }
 }
